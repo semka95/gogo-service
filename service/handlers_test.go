c8ea224042a7daaf53aede4b07b46a287c04d895
semka95 <semen.zernov@live.ru>
2017-01-20 20:55:48 +0300

    Create test, that invokes createMatchHendler function. Create that function.

---
+++ service/handlers_test.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/unrolled/render"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var (
+	formatter = render.New(render.Options{
+		IndentJSON: true,
+	})
+)
+
+func TestCreateMatch(t *testing.T) {
+	client := &http.Client{}
+	server := httptest.NewServer(
+		http.HandlerFunc(createMatchHandler(formatter)))
+	defer server.Close()
+	body := []byte("{\n \"gridsize\": 19,\n \"players\": [\n {\n\"color\": \"white\",\n \"name\": \"bob\"\n },\n {\n\"color\": \"black\",\n \"name\": \"alfred\"\n }\n ]\n}")
+	req, err := http.NewRequest("POST",
+		server.URL, bytes.NewBuffer(body))
+	if err != nil {
+		t.Errorf("Error in creating POST request for createMatchHandler: %v",
+			err)
+	}
+	req.Header.Add("Content-Type", "application/json")
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error in POST to createMatchHandler: %v", err)
+	}
+	defer res.Body.Close()
+	payload, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+	if res.StatusCode != http.StatusCreated {
+		t.Errorf("Expected response status 201, received %s",
+			res.Status)
+	}
+	fmt.Printf("Payload: %s", string(payload))
+}
