2e32be7c06ef06b576c80330a1796755d81dcc71
semka95 <semen.zernov@live.ru>
2017-01-20 21:08:03 +0300

    Add an assertion that Location header has proper format. Add generating new GUID and setting proper Location header in createMatchHandler function

--- service/handlers.go
+++ service/handlers.go
@@ -1,13 +1,16 @@
 package service
 
 import (
+	"github.com/pborman/uuid"
+
 	"github.com/unrolled/render"
 	"net/http"
 )
 
 func createMatchHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Location", "some value")
+		guid := uuid.New()
+		w.Header().Add("Location", "/matches/"+guid)
 		formatter.JSON(w,
 			http.StatusCreated,
 			struct{ Test string }{"This is a test"})
--- service/handlers_test.go
+++ service/handlers_test.go
@@ -7,7 +7,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+)
+
+const (
+	fakeMatchLocationResult = "/matches/5a003b78-409e-4452-b456-a6f0dcee05bd"
 )
 
 var (
@@ -21,7 +26,9 @@ func TestCreateMatch(t *testing.T) {
 	server := httptest.NewServer(
 		http.HandlerFunc(createMatchHandler(formatter)))
 	defer server.Close()
+
 	body := []byte("{\n \"gridsize\": 19,\n \"players\": [\n {\n\"color\": \"white\",\n \"name\": \"bob\"\n },\n {\n\"color\": \"black\",\n \"name\": \"alfred\"\n }\n ]\n}")
+
 	req, err := http.NewRequest("POST",
 		server.URL, bytes.NewBuffer(body))
 	if err != nil {
@@ -29,22 +36,35 @@ func TestCreateMatch(t *testing.T) {
 			err)
 	}
 	req.Header.Add("Content-Type", "application/json")
+
 	res, err := client.Do(req)
 	if err != nil {
 		t.Errorf("Error in POST to createMatchHandler: %v", err)
 	}
+
 	defer res.Body.Close()
+
 	payload, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		t.Errorf("Error reading response body: %v", err)
 	}
+
 	if res.StatusCode != http.StatusCreated {
 		t.Errorf("Expected response status 201, received %s",
 			res.Status)
 	}
 
-	if res.Header["Location"] == nil {
+	loc := res.Header["Location"]
+	if loc == nil {
 		t.Error("Location header is not set")
+	} else {
+		if !strings.Contains(loc[0], "/matches/") {
+			t.Errorf("Location header should contain '/matches/'")
+		}
+		if len(loc[0]) != len(fakeMatchLocationResult) {
+			t.Errorf("Location value does not contain guid of new match")
+		}
 	}
+
 	fmt.Printf("Payload: %s", string(payload))
 }
